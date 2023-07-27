33cc896b5da8ae2aeed4271c99eb959bf014d733
daniyal-haider-brainx <daniyal.haider@brainxtech.com>
2023-07-27 10:56:53 +0500

    Code Review Changes

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,41 +1,40 @@
 // Get the to-do form element
 const todoForm = document.getElementById('todoForm');
 // Get the input field for adding to-do items
-const todoInput = document.getElementById('todoInput');
+const addItem = document.getElementById('todoInput');
 // Get the list element to display to-do items
-const todoList = document.getElementById('todoList');
-
+const ItemList = document.getElementById('todoList');
 todoForm.addEventListener('submit', function (event) {
   // Prevent form submission
   event.preventDefault();
   // Get the trimmed value of the input field
-  const todoText = todoInput.value.trim();
-  if (todoText !== '') {
+  const description = addItem.value.trim();
+  if (description !== '') {
     // Create a new to-do item
-    const todoItem = createTodoItem(todoText);
+    const ListItem = createList(description);
     // Add the new to-do item to the list
-    todoList.appendChild(todoItem);
+    ItemList.appendChild(ListItem);
     // Clear the input field
-    todoInput.value = '';
+    addItem.value = '';
   }
 });
 
-function createTodoItem(todoText) {
+function createList(description) {
   // Create a new list item element for the to-do item
-  const todoItem = document.createElement('li');
-  todoItem.classList.add('todo-item');
+  const ListItem = document.createElement('li');
+  ListItem.classList.add('todo-item');
   // Create an input element for the checkbox
-  const todoCheckbox = document.createElement('input');
+  const checkedItem = document.createElement('input');
   // Set the type to 'checkbox'
-  todoCheckbox.type = 'checkbox';
-  todoCheckbox.addEventListener('change', function () {
+  checkedItem.type = 'checkbox';
+  checkedItem.addEventListener('change', function () {
     // Call the checkBoxChanged function when the checkbox is changed
-    checkBoxChanged(todoItem);
+    checkBoxChanged(ListItem);
   });
   // Create a span element for the to-do text
-  const todoTextSpan = document.createElement('span');
+  const descriptionArea = document.createElement('span');
   // Set the text content of the span element
-  todoTextSpan.textContent = todoText;
+  descriptionArea.textContent = description;
   // Create a button element for editing the to-do item
   const editButton = document.createElement('button');
   // Set the text content of the button
@@ -43,7 +42,7 @@ function createTodoItem(todoText) {
   editButton.classList.add('edit-button');
   // Call the editTodoItem function when the button is clicked
   editButton.addEventListener('click', function () {
-    editTodoItem(todoItem);
+    editTodoItem(ListItem);
   });
   // Create a button element for removing the to-do item
   const removeButton = document.createElement('button');
@@ -52,70 +51,70 @@ function createTodoItem(todoText) {
   removeButton.classList.add('remove-button');
   // Call the removeTodoItem function when the button is clicked
   removeButton.addEventListener('click', function () {
-    removeTodoItem(todoItem);
+    removeTodoItem(ListItem);
   });
 
   // Append the elements to the to-do item list item
-  todoItem.appendChild(todoCheckbox);
-  todoItem.appendChild(todoTextSpan);
-  todoItem.appendChild(editButton);
-  todoItem.appendChild(removeButton);
+  ListItem.appendChild(checkedItem);
+  ListItem.appendChild(descriptionArea);
+  ListItem.appendChild(editButton);
+  ListItem.appendChild(removeButton);
   // Return the created to-do item
-  return todoItem;
+  return ListItem;
 }
 
-function checkBoxChanged(todoItem) {
+function checkBoxChanged(ListItem) {
   // Select the checkbox input element within the todoItem
-  const checkbox = todoItem.querySelector('input');
+  const checkbox = ListItem.querySelector('input');
   // Select the span element containing the todo text
-  const todoTextSpan = todoItem.querySelector('span');
+  const descriptionArea = ListItem.querySelector('span');
 
   if (checkbox.checked) {
-    todoTextSpan.style.textDecoration = 'line-through';
-    todoTextSpan.classList.add('completed');
+    descriptionArea.style.textDecoration = 'line-through';
+    descriptionArea.classList.add('completed');
   } else {
-    todoTextSpan.style.textDecoration = 'none';
-    todoTextSpan.classList.remove('completed');
+    descriptionArea.style.textDecoration = 'none';
+    descriptionArea.classList.remove('completed');
   }
 }
 
-function editTodoItem(todoItem) {
+function editTodoItem(ListItem) {
   // Get the checkbox input element within the todoItem
-  const checkbox = todoItem.querySelector('input');
+  const checkbox = ListItem.querySelector('input');
   // Get the span element containing the to-do text
-  const todoTextSpan = todoItem.querySelector('span');
+  const descriptionArea = ListItem.querySelector('span');
   // Get the current text content
-  const todoText = todoTextSpan.textContent;
+  const description = descriptionArea.textContent;
 
   // Create an input element for editing the text
   const editInput = document.createElement('input');
   // Set the type to 'text'
   editInput.type = 'text';
   // Set the value of the input field to the current text
-  editInput.value = todoText;
+  editInput.value = description;
 
   // Create a button element for saving the edited text
   const saveButton = document.createElement('button');
   // Set the text content of the button
   if (checkbox.checked) {
-    todoTextSpan.style.textDecoration = 'line-through';
-    todoTextSpan.classList.add('completed');
+    descriptionArea.style.textDecoration = 'line-through';
+    descriptionArea.classList.add('completed');
   } else {
-    todoTextSpan.style.textDecoration = 'none';
-    todoTextSpan.classList.remove('completed');
+    descriptionArea.style.textDecoration = 'none';
+    descriptionArea.classList.remove('completed');
   }
   saveButton.textContent = 'Save';
   saveButton.classList.add('save-button');
   // Call the saveEditedTodoItem function when the button is clicked
   saveButton.addEventListener('click', function () {
-    saveEditedTodoItem(todoItem, editInput, checkbox.checked);
+    saveEditedTodoItem(ListItem, editInput, checkbox.checked);
   });
 
   // Replace the to-do item contents with the edit input field and save button
-  todoItem.innerHTML = '';
-  todoItem.appendChild(checkbox);
-  todoItem.appendChild(editInput);
-  todoItem.appendChild(saveButton);
+  ListItem.innerHTML = '';
+  ListItem.appendChild(checkbox);
+  ListItem.appendChild(editInput);
+  ListItem.appendChild(saveButton);
 
   // Check the checkbox if it was previously checked
   if (checkbox.checked) {
@@ -123,30 +122,30 @@ function editTodoItem(todoItem) {
   }
 }
 
-function saveEditedTodoItem(todoItem, editInput, isChecked) {
+function saveEditedTodoItem(ListItem, editInput, isChecked) {
   // Get the trimmed value of the edit input field
   const newTodoText = editInput.value.trim();
   if (newTodoText !== '') {
     // Create a new span element for the updated text
-    const todoTextSpan = document.createElement('span');
+    const descriptionArea = document.createElement('span');
     // Set the text content of the span element
-    todoTextSpan.textContent = newTodoText;
+    descriptionArea.textContent = newTodoText;
 
     // Create an input element for the checkbox
-    const todoCheckbox = document.createElement('input');
+    const checkedItem = document.createElement('input');
     // Set the type to 'checkbox'
-    todoCheckbox.type = 'checkbox';
-    todoCheckbox.addEventListener('change', function () {
+    checkedItem.type = 'checkbox';
+    checkedItem.addEventListener('change', function () {
       // Call the checkBoxChanged function when the checkbox is changed
-      checkBoxChanged(todoItem);
+      checkBoxChanged(ListItem);
     });
     // Set the checkbox state to the previous state
-    todoCheckbox.checked = isChecked;
+    checkedItem.checked = isChecked;
 
     // Check if the checkbox is checked to apply line-through class
     if (isChecked) {
-      todoTextSpan.style.textDecoration = 'line-through';
-      todoTextSpan.classList.add('completed');
+      descriptionArea.style.textDecoration = 'line-through';
+      descriptionArea.classList.add('completed');
     }
 
     // Create a button element for editing the updated to-do item
@@ -156,7 +155,7 @@ function saveEditedTodoItem(todoItem, editInput, isChecked) {
     editButton.classList.add('edit-button');
     editButton.addEventListener('click', function () {
       // Call the editTodoItem function when the button is clicked
-      editTodoItem(todoItem);
+      editTodoItem(ListItem);
     });
 
     // Create a button element for removing the updated to-do item
@@ -166,20 +165,18 @@ function saveEditedTodoItem(todoItem, editInput, isChecked) {
     removeButton.classList.add('remove-button');
     removeButton.addEventListener('click', function () {
       // Call the removeTodoItem function when the button is clicked
-      removeTodoItem(todoItem);
+      removeTodoItem(ListItem);
     });
 
     // Replace the to-do item contents with the updated elements
-    todoItem.innerHTML = '';
-    todoItem.appendChild(todoCheckbox);
-    todoItem.appendChild(todoTextSpan);
-    todoItem.appendChild(editButton);
-    todoItem.appendChild(removeButton);
+    ListItem.innerHTML = '';
+    ListItem.appendChild(checkedItem);
+    ListItem.appendChild(descriptionArea);
+    ListItem.appendChild(editButton);
+    ListItem.appendChild(removeButton);
   }
 }
-
-
-function removeTodoItem(todoItem) {
+function removeTodoItem(ListItem) {
   // Remove the to-do item from the list
-  todoItem.remove();
+  ListItem.remove();
 }
